df2ef7dec763f8aa9c47fbc812a0d011a406a7b5
Kuldeepsaini0434 <kuldeepsainimi4@gmai.com>
2026-05-20 22:43:51 +0530

    Fixed CORS for deployment

--- server/server.js
+++ server/server.js
@@ -10,12 +10,21 @@ const taskRoutes = require("./routes/taskRoutes");
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+
+    methods: ["GET", "POST", "PUT", "DELETE"],
+
+    credentials: true,
+  }),
+);
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI)
-.then(() => console.log("MongoDB Connected"))
-.catch((err) => console.log(err));
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.log(err));
 
 app.use("/api/auth", authRoutes);
 
@@ -23,12 +32,12 @@ app.use("/api/projects", projectRoutes);
 
 app.use("/api/tasks", taskRoutes);
 
-app.get("/", (req,res)=>{
-   res.send("API Running");
+app.get("/", (req, res) => {
+  res.send("API Running");
 });
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, ()=>{
-   console.log(`Server running on port ${PORT}`);
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
